fd321a8e9d189eced2084ab278430df43b8bdae1
Branko <olujic.branko@gmail.com>
2022-09-17 13:56:11 +0200

    levels and google docs

--- Controller/cell.py
+++ Controller/cell.py
@@ -3,16 +3,18 @@ import random
 from kivymd.uix.button import MDFlatButton
 from kivy.metrics import dp
 
-from Controller.settings import CELL_COUNT, NUMBER_OF_MINES
+from Controller import settings
 from View.Manager.info_manager import InfoManager
 from Model.user_manager import user_manager
 
 
 class Cell(object):
-    """Cell class for creating base cell object containing mine, if decided by random function."""
+    """
+    Cell class for creating base cell object containing mine, if decided by random function.
+    """
     _instances = list()
-    cell_count = CELL_COUNT
-    mine_count = NUMBER_OF_MINES
+    cell_count = settings.CELL_COUNT
+    mine_count = settings.CELL_COUNT // settings.BEGINNER
 
     def __init__(self, x, y, is_mine=False):
         self.x, self.y = x, y
@@ -47,24 +49,39 @@ class Cell(object):
 
     @classmethod
     def shuffle_mines(cls):
-        """Called before start of new game to shuffle mines on table."""
+        """
+        Called before start of new game to shuffle mines on table.
+        Returns:
+            None.
+        """
         for instance in cls._instances:
             instance.is_mine = False
             instance.is_open = False
             instance.cell_object.text = ''
             instance.cell_object.disabled = False
+        cls.cell_count = settings.CELL_COUNT
+        cls.mine_count = settings.CELL_COUNT // settings.BEGINNER
         cls.randomize_mines()
-        cls.cell_count = CELL_COUNT
-        cls.mine_count = NUMBER_OF_MINES
 
     @classmethod
     def randomize_mines(cls):
-        """Called to randomly select cells with mines."""
-        for cell in random.sample(cls._instances, NUMBER_OF_MINES):
+        """
+        Called to randomly select cells with mines.
+        Returns:
+            None.
+        """
+        for cell in random.sample(cls._instances, cls.mine_count):
             cell.is_mine = True
 
     def game_over(self, app):
-        """Called when you step on mine."""
+        """
+        Called when you step on mine.
+        Args:
+            app (MDApp): represents app object
+
+        Returns:
+            None: if no exceptions are raised inside context manager return None
+        """
         InfoManager().game_over_info()
         with user_manager as manager:
             manager.record_loss(app.player)
@@ -72,7 +89,14 @@ class Cell(object):
             cell.cell_object.disabled = True
 
     def create_button(self, position):
-        """Instantiate button object and assign it to instance.cell_object."""
+        """
+        Instantiate button object and assign it to instance.cell_object.
+        Args:
+            position (tuple): position of the cell in grid layout.
+
+        Returns:
+            None
+        """
         self.cell_object = MDFlatButton(pos=position,
                                         on_press=self.open_cell,
                                         on_release=self.refresh_label,
@@ -80,7 +104,14 @@ class Cell(object):
                                         font_size=dp(16))
 
     def open_cell(self, widget):
-        """Open cell object."""
+        """
+        Open cell object.
+        Args:
+            widget (MDFlatButton): represent button from which this function is triggered.
+
+        Returns:
+            None.
+        """
         main_screen = widget.parent.parent.parent
         if main_screen.mine_flag:
             self.mark_mine(widget)
@@ -95,24 +126,44 @@ class Cell(object):
             self.show_cell()
 
     def refresh_label(self, widget):
-        """Refresh labels after opening cell."""
+        """
+        Refresh labels after opening cell.
+        Args:
+            widget (MDFlatButton): represent button from which this function is triggered.
+
+        Returns:
+            None.
+        """
         main_screen = widget.parent.parent.parent
         cell_label = main_screen.ids.cell_counter
         cell_label.text = f'Cells left: {self.cell_count}'
         mine_label = main_screen.ids.mine_counter
         mine_label.text = f'Mines left: {self.mine_count}'
-        if Cell.cell_count == NUMBER_OF_MINES:
+        if self.cell_count == settings.CELL_COUNT // settings.BEGINNER:
             InfoManager().win_info()
             with user_manager as manager:
                 manager.record_win(main_screen.manager.app.player)
 
     def get_cell(self, x, y):
+        """
+        Function for retrieving a cell object based on it`s position.
+        Args:
+            x (int): represent x-axis in cell`s position
+            y (int): represent y-axis in cell`s position
+
+        Returns:
+            Cell object.
+        """
         for cell in self._instances:
             if cell.x == x and cell.y == y:
                 return cell
 
     def show_cell(self):
-        """Show cell info."""
+        """
+        Show cell info.
+        Returns:
+            None.
+        """
         if not self.is_open:
             Cell.cell_count -= 1
             if self.cell_object.text == '*':
@@ -122,7 +173,14 @@ class Cell(object):
         self.cell_object.disabled = True
 
     def mark_mine(self, widget):
-        """Called when you want to mark a cell as a mine."""
+        """
+        Called when you want to mark a cell as a mine.
+        Args:
+            widget (MDFlatButton): represent button from which this function is triggered.
+
+        Returns:
+            None.
+        """
         if not self.cell_object.text:
             Cell.mine_count -= 1
             self.cell_object.text = '*'
